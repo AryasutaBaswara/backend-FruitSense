ba67ed86becd07b3fd35478f0e5a15129bd540d6
AryasutaBaswara <baswarathunder22@gmail.com>
2025-10-11 13:33:45 +0700

    fcm firebase, belum testing dengan FE

--- services/fcmService.js
+++ services/fcmService.js
@@ -1,8 +1,41 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("../firebase-credentials.json");
+const { response } = require("express");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+/**
+ *
+ * @param {string} token
+ * @param {string} title
+ * @param {string} body
+ */
+
 exports.sendNotification = (token, title, body) => {
-  console.log(`\n[PUSH SIMULASI] Peringatan Buah Busuk:`);
-  console.log(`  Token Tujuan: ${token.substring(0, 10)}...`);
-  console.log(`  Judul: ${title}`);
-  console.log(`  Body: ${body}`);
-  // Nanti diganti dengan return client.send(message);
-  return { success: true, message: "Simulasi berhasil" };
+  const message = {
+    notification: {
+      title: title,
+      body: body,
+    },
+    token: token,
+  };
+
+  return admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      console.log(`\n[FCM NYATA] Notifikasi berhasil dikirim:`, response);
+      return response;
+    })
+    .catch((error) => {
+      console.error(
+        `\n[FCM ERROR] Gagal mengirim notifikasi via FCM.`,
+        error.errorInfo
+      );
+      // CATATAN: Dalam aplikasi nyata, Anda harus menghapus token yang invalid dari database.
+      return { success: false, error: error.errorInfo };
+    });
 };
